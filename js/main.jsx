4e6890ba3006972165d87249a7c30facd190017f
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-11-19 09:34:02 -0800

    nothing

--- js/main.jsx
+++ js/main.jsx
@@ -12,28 +12,3 @@ var RegisterForm = require('./components/registerForm.jsx');
 ReactDOM.render(<LoginRegister />, document.getElementById('loginRegister'));
 ReactDOM.render(<RegisterForm />, document.getElementById('registerForm'));
 
-// var Movie = Backbone.Model.extend({
-//   initialize: function () {
-//     console.log("A new movie has been created");
-//   }
-
-// var Movies=Backbone.Collection.extend({
-// url:"https://test1117.parseapp.com/classes/Movies",
-// model: Movie,
-// _parse_class_name:"Movies"
-
-// });
-
-// test.save(null, {
-//   success: function(resp) {
-//     console.log("success: ",resp)
-//   },
-//   error: function(err) {
-//     console.log("error: ",err)
-//   }
-// });
-
-// var test = new Movie({
-// 	title:test,
-// 	director: test,
-// })
